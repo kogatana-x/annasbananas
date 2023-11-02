1fd48f9f4bc363e9bdd8852ee8ede0e2e2e80e92
kogatana-x <kogatana.x@gmail.com>
2023-11-02 12:42:14 -0500

    New branch layout. Init Project Files

---
+++ Final Project/Final.java
@@ -0,0 +1,61 @@
+import java.util.List;
+class User {
+    private String username;
+    private String password;
+    private Cart cart;
+    // getters and setters
+}
+
+class Product {
+    private String id;
+    private String name;
+    private String description;
+    private double price;
+    // getters and setters
+}
+
+class Cart {
+    private List<Product> products;
+    // methods to add and remove products
+}
+
+class UserAuthenticator {
+    private UserRepository userRepository;
+    // methods to log in and log out users
+}
+
+class ProductCatalog {
+    private ProductRepository productRepository;
+    // methods to add, remove, and retrieve products
+}
+
+class UserRepository {
+    // implementation of UserRepository
+}
+
+class ProductRepository {
+    // implementation of ProductRepository
+    // methods to add, remove, and retrieve products
+}
+
+class OrderProcessor {
+    private Cart cart;
+    private PaymentProcessor paymentProcessor;
+    // method to place orders
+}
+
+class PaymentProcessor {
+    // method to process payments
+}
+
+class Logger {
+    private static Logger instance;
+    // methods to log events and transactions
+    // private constructor and public static getInstance() method to implement the singleton pattern
+}
+
+public class Final{
+    public static void main(String[] args) {
+        
+    }
+}
