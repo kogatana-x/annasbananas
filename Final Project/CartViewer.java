f50cc055b79d97ff9afa311b74bea08577724baf
kogatana-x <kogatana.x@gmail.com>
2023-11-11 21:39:02 -0600

    test

--- Final Project/CartViewer.java
+++ Final Project/CartViewer.java
@@ -7,5 +7,18 @@ public class CartViewer {
 
     public void displayCart(){
         //takes cart.id and displays all products in cart
+        //cart.id;
+        //cart.products;
+        ProductCatalog pc = new ProductCatalog(new ProductRepository());
+        String[] cartProducts = cart.getCart();
+
+        System.out.println("Cart ID: " + cartProducts[0]);
+        for(int x=0; x<cartProducts.length; x++){
+            if(cartProducts[x].equals("0")){
+                continue;
+            }
+            Product product = pc.getProduct(Integer.toString(x));
+        }
+
     }
 }
---
+++ Final Project/HTMLDisplay.java
@@ -0,0 +1,8 @@
+import java.net.Socket;
+
+public class HTMLDisplay extends HTMLParser{
+    public HTMLDisplay(Socket socket){
+        super(socket);
+    }
+    
+}
